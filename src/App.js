c956d89836637991fc3d9d8e16cecfc0d37cb735
ozlemkilic <ozlemkilic93@gmail.com>
2021-08-14 18:30:23 +0300

    ozlem | converted to class component and added unit test for DOM

--- src/App.js
+++ src/App.js
@@ -1,105 +1,115 @@
-import { useEffect, useState } from 'react';
+import React, {Component} from 'react';
 import './style.scss';
-import image from './assets/images/top_image_scale_1x.png';
-import imageRetina from './assets/images/top_image_scale_2x.png';
+import image1x from './assets/images/top_image_scale_1x.png';
+import image2x from './assets/images/top_image_scale_2x.png';
 
-function App() {
-    const [data, setData]=useState({});
-    const timeFromStorage = JSON.parse(window.localStorage.getItem('time')) || {};
-    const [time, setTime] = useState({
-        hours: timeFromStorage.hours || data.duration_hour || 0,
-        minutes: timeFromStorage.minutes || 0,
-        seconds: timeFromStorage.seconds || 0
-    });
-    const [isCompleted, setCountdownStatus] = useState(false);
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.timeFromStorage = JSON.parse(window.localStorage.getItem('time')) || {};
+        this.state = {
+            data: {},
+            time: {
+                hours: this.timeFromStorage.hours || 0,
+                minutes: this.timeFromStorage.minutes || 0,
+                seconds: this.timeFromStorage.seconds || 0
+            },
+            isCompleted: false
+        };
+        this.intervalId = null;
+    }
 
-    const startTimer = () => {
-        if (time.hours === 0 && time.minutes === 0 && time.seconds === 0) {
-            setCountdownStatus(true);
-        } else if (time.minutes === 0 && time.seconds === 0) {
-            setTime({
-                hours: time.hours - 1,
-                minutes: 59,
-                seconds: 59
+    readFile = () => {
+        fetch('data.json')
+            .then(response => { response.json()
+                .then(data => { this.setState({ data }); })
+                .catch(() => this.setState({ data: { duration_hour: 0, url: '', cash_value: 0 } }));
             });
-        } else if (time.seconds === 0) {
-            setTime({
-                hours: time.hours,
-                minutes: time.minutes - 1,
-                seconds: 59
+
+        this.setState({ time: {
+                hours: this.timeFromStorage.hours || this.state.data.duration_hour || 0,
+                minutes: this.timeFromStorage.minutes || 0,
+                seconds: this.timeFromStorage.seconds || 0
+            }}, () => {
+                this.intervalId = setInterval(() => this.startTimer(), 1000);
+            })
+    };
+    startTimer = () => {
+        if (this.state.time.hours === 0 && this.state.time.minutes === 0 && this.state.time.seconds === 0) {
+            this.setState({ isCompleted: true });
+        } else if (this.state.time.minutes === 0 && this.state.time.seconds === 0) {
+            this.setState({
+                time: {
+                    hours: this.state.time.hours - 1,
+                    minutes: 59,
+                    seconds: 59
+                }
+            });
+        } else if (this.state.time.seconds === 0) {
+            this.setState({
+                time: {
+                    hours: this.state.time.hours,
+                    minutes: this.state.time.minutes - 1,
+                    seconds: 59
+                }
             });
         } else {
-            setTime({
-                hours: time.hours,
-                minutes: time.minutes,
-                seconds: time.seconds - 1
+            this.setState({
+                time: {
+                    hours: this.state.time.hours,
+                    minutes: this.state.time.minutes,
+                    seconds: this.state.time.seconds - 1
+                }
             });
         }
+
+        window.localStorage.setItem('time', JSON.stringify(this.state.time));
     };
-    const addPad = data => data.toString().padStart(2, 0);
-    const readFile = () => {
-        import('./data')
-            .then(({ data }) => {
-                setData(data);
-            })
-            .catch(() => setData({ duration_hour: 0, url: '', cash_value: 0 }));
-    };
+    addPad = data => data.toString().padStart(2, 0);
 
-    useEffect(() => {
-        readFile();
-    }, []);
-    useEffect(() => {
-        setTime({
-            hours: timeFromStorage.hours || data.duration_hour || 0,
-            minutes: timeFromStorage.minutes || 0,
-            seconds: timeFromStorage.seconds || 0
-        });
-    }, [data]);
-    useEffect(() => {
-        let intervalId = null;
+    componentDidMount() {
+        this.readFile();
+    }
 
-        if (typeof data === 'object' && Object.keys(data).length !== 0) {
-            intervalId = setInterval(() => startTimer(), 1000);
+    componentWillUnmount() {
+        clearInterval(this.intervalId);
+    }
 
-            window.localStorage.setItem('time', JSON.stringify(time));
-        }
-
-        return () => clearInterval(intervalId);
-    }, [time]);
-
-    return (
-        <div className="countdown">
-            <section className="container">
-                {!isCompleted &&
+    render() {
+        return (
+            <div className="countdown">
+                <section className="container">
+                    {!this.state.isCompleted &&
                     <div className="description">
                         <img
-                            src={image}
-                            srcSet={`${image} 1x, ${imageRetina} 2x`}
+                            src={image1x}
+                            srcSet={`${image1x} 1x, ${image2x} 2x`}
                             alt="top-image"
                         />
-                        <p>Get your free £{data.cash_value || 0} now</p>
+                        <p>Get your free £{this.state.data.cash_value || 0} now</p>
                     </div>
-                }
-                <div className="duration">
-                    <div>
-                        <p>{ addPad(time.hours) }</p>
-                        <p className="duration-text">Hours</p>
+                    }
+                    <div className="duration">
+                        <div className="hours">
+                            <p>{ this.addPad(this.state.time.hours) }</p>
+                            <span className="duration-text">Hours</span>
+                        </div>
+                        <span>:</span>
+                        <div className="minutes">
+                            <p>{ this.addPad(this.state.time.minutes) }</p>
+                            <span className="duration-text">Minutes</span>
+                        </div>
+                        <span>:</span>
+                        <div className="seconds">
+                            <p>{ this.addPad(this.state.time.seconds) }</p>
+                            <span className="duration-text">Seconds</span>
+                        </div>
                     </div>
-                    <span>:</span>
-                    <div>
-                        <p>{ addPad(time.minutes) }</p>
-                        <p className="duration-text">Minutes</p>
-                    </div>
-                    <span>:</span>
-                    <div>
-                        <p>{ addPad(time.seconds) }</p>
-                        <p className="duration-text">Seconds</p>
-                    </div>
-                </div>
-                {!isCompleted && <a href={data.url || ''} target="_blank">Opt-in</a>}
-            </section>
-        </div>
-    );
-};
+                    {!this.state.isCompleted && <a href={this.state.data.url || ''} target="_blank">Opt-in</a>}
+                </section>
+            </div>
+        );
+    }
+}
 
 export default App;
--- src/App.spec.js
+++ src/App.spec.js
@@ -9,7 +9,50 @@ describe("App.js", () => {
         wrapper = shallow(<App />);
     });
 
-    test('shallow wrapper instance should not be null', () => {
-        expect(true).toBeTruthy();
+    it('should render', () => {
+        expect(wrapper).not.toBeNull();
+    });
+
+    describe('DOM', () => {
+        it('should have a description area', () => {
+            expect(wrapper.find('.description').length).toEqual(1);
+        });
+
+        it('should have not a description area', async () => {
+            wrapper.setState({ isCompleted: true });
+
+            expect(wrapper.find('.description').exists()).toBeFalsy();
+        });
+
+        it('should have `a` tag', () => {
+            expect(wrapper.find('a').length).toEqual(1);
+        });
+
+        it('should have not `a` tag', async () => {
+            wrapper.setState({ isCompleted: true });
+
+            expect(wrapper.find('a').exists()).toBeFalsy();
+        });
+
+        it('should have correct url', async () => {
+            const url = 'www.googgle.com';
+            wrapper.setState({ data: { url } });
+
+            expect(wrapper.find('a').props().href).toEqual(url);
+        });
+
+        it('should have correct offer text', async () => {
+            wrapper.setState({ data: { cash_value: 10 } });
+
+            expect(wrapper.find('.description p').text()).toContain('10');
+        });
+
+        it('should have correct duration', async () => {
+            wrapper.setState({ time: { hours: 1, minutes: 35, seconds: 20 } });
+
+            expect(wrapper.find('.hours p').at(0).text()).toEqual('01');
+            expect(wrapper.find('.minutes p').at(0).text()).toEqual('35');
+            expect(wrapper.find('.seconds p').at(0).text()).toEqual('20');
+        });
     });
 });
